d164f8b40af8aee1fe255a67aaf4dc13c42207b2
Jaideep Sundaram <jsundaram@pgdx.com>
2018-07-25 10:18:01 -0400

    initial check-in

--- merck/immunoselect/matched/report.py
+++ merck/immunoselect/matched/report.py
@@ -1,5 +1,7 @@
 import pgdx.report
 
+import openpyxl
+
 class ReportGenerator(pgdx.report.ReportGenerator):
     """
     
@@ -11,10 +13,52 @@ class ReportGenerator(pgdx.report.ReportGenerator):
         """
         self._trigger_file = trigger_file
         self._outdir = outdir
+        self._template_directory = 'templates'
+        self._excel_template_name = 'Merck_ImmunoSELECT_Matched_Report_Template.xlsx'
+        self._template_file = self._template_directory + '/' + self._excel_template_name
+        self._outfile = self._outdir + self._excel_template_name
 
     def generateReport(self):
         """
 
         :return:
         """
-        print("Generated report")
+        self._xfile = openpyxl.load_workbook(self._template_file)
+
+        self._write_overview_sheet()
+
+        self._write_results_summary_sheet()
+
+        self._xfile.save(self._outfile)
+
+        print("Wrote output file '%s'" % self._outfile)
+
+    def _write_overview_sheet(self):
+        """
+
+        :return:
+        """
+        sheet_name = 'Overview'
+
+        sheet = self._xfile.get_sheet_by_name(sheet_name)
+
+        sheet['A14'] = "test"
+
+        print("Wrote to sheet '%s'" % sheet_name)
+
+        # pass
+
+    def _write_results_summary_sheet(self):
+        """
+
+        :return:
+        """
+        sheet_name = 'Results summary'
+
+        sheet = self._xfile.get_sheet_by_name(sheet_name)
+        
+        sheet['A14'] = "test"
+
+        print("Wrote to sheet '%s'" % sheet_name)
+
+        # pass
